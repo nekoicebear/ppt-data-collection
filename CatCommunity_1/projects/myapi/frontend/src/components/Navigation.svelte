501cb616b4ab7c9e399a86822bfabcb526b0320d
nekoicebear <nekoicebear@hs.ac.kr>
2023-11-15 16:51:03 +0900

    catcommunity_1 에 대한 커밋이며, 프론트 아직 완벽하게 하지는 않음. 추후 프론트 꾸민 후 다시 커밋 예정

---
+++ CatCommunity_1/projects/myapi/frontend/src/components/Navigation.svelte
@@ -0,0 +1,47 @@
+<script>
+    import { link } from 'svelte-spa-router'
+    import { page, keyword, access_token, username, is_login } from "../lib/store"
+</script>
+
+<!-- 네비게이션바 -->
+<style>
+    .navbar-custom {
+        background-color: #ffae008e; /* 배경 색상을 원하는 색상으로 지정하세요. */
+    }
+</style>
+
+<nav class="navbar navbar-expand-lg navbar-light navbar-custom border-bottom">
+    <div class="container-fluid">
+        <a use:link class="navbar-brand" href="/" on:click="{() => {$keyword = '', $page =0 }}">고양이 커뮤니티</a>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon" />
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                {#if $is_login }
+                    <li class="nav-item">
+                        <a use:link href="/user-login" class="nav-link" on:click={() => {
+                            $access_token = ''
+                            $username = ''
+                            $is_login = false
+                        }}>로그아웃 ({$username})</a>
+                    </li>
+                {:else}
+                    <li class="nav-item">
+                        <a use:link class="nav-link" href="/user-create">회원가입</a>
+                    </li>
+                    <li class="nav-item">
+                        <a use:link class="nav-link" href="/user-login">로그인</a>
+                    </li>
+                {/if}
+            </ul>
+        </div>
+    </div>
+</nav>
